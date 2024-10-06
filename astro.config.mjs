d370ff2b7f26da514f17a33ef6e1df2da0f295db
Santosh Yadav <ymanoutube@gmail.com>
2024-10-06 14:19:17 +0545

    deployment specific changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,14 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: true,
   }), react()],
+
+  output: "server",
+  adapter: vercel(),
 });
